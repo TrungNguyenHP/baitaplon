25ebe5094230cc20e66d1759423e9a0611fcb3ab
TrungNguyenHP <152580377+TrungNguyenHP@users.noreply.github.com>
2024-09-03 17:19:29 +0700

    Update connection.php

--- backend/connection.php
+++ backend/connection.php
@@ -1,8 +1,8 @@
 <?php 
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "watchshop";
+    $server = "dongho1.mysql.database.azure.com";
+    $user = "dongho";
+    $password = "Hieu0411";
+    $database = "dong_ho";
 
     $conn = new mysqli($server, $user, $password, $database);
 
